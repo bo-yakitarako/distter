1a47836fffdc4356b7c7646c6f041197b909cda3
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2022-10-29 20:51:41 +0900

    試しにトップページでも作るべ

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('なんにもないよ');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // eslint-disable-line @typescript-eslint/naming-convention
+const __dirname = path.dirname(__filename); // eslint-disable-line @typescript-eslint/naming-convention
+
+const server = express();
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+const router = express.Router();
+
+server.use('/', express.static(`${__dirname}/../public`));
+
+router.get('/', (req, res) => {
+  res.render('../ejs/index.ejs');
+});
+
+server.use('/', router);
+
+server.listen(3000, () => {
+  console.log('サーバー起動しますわよ！');
+});
