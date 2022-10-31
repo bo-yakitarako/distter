9a42046b93ab8a599be0f282b181431f82841f33
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2022-11-01 03:24:00 +0900

    うおおおおおおおおおお俺の価値だあああ

--- src/encrypt.ts
+++ src/encrypt.ts
@@ -6,13 +6,14 @@ config();
 const algorithm = 'aes-256-cbc';
 const salt = process.env.PG_USERNAME as string;
 const password = process.env.PG_PASSWORD as string;
+const ivString = process.env.IV as string;
 
+const iv = Buffer.from(ivString, 'hex');
 const inputEncoding = 'utf8';
 const outputEncoding = 'hex';
 
 export const encrypt = (text: string) => {
   const key = crypto.scryptSync(password, salt, 32);
-  const iv = crypto.randomBytes(16);
   const cipher = crypto.createCipheriv(algorithm, key, iv);
   const data = cipher.update(text, inputEncoding, outputEncoding);
   const final = cipher.final(outputEncoding);
@@ -21,7 +22,6 @@ export const encrypt = (text: string) => {
 
 export const decrypt = (text: string) => {
   const key = crypto.scryptSync(password, salt, 32);
-  const iv = crypto.randomBytes(16);
   const decipher = crypto.createDecipheriv(algorithm, key, iv);
   const data = decipher.update(text, outputEncoding, inputEncoding);
   const final = decipher.final(inputEncoding);
