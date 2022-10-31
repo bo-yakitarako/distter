53db46e57268c4826380b2696a889b1965758879
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2022-10-31 20:01:13 +0900

    なんだおまえ

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,11 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { APIEmbed, Client, GatewayIntentBits, Message } from 'discord.js';
 import { config } from 'dotenv';
+import { query } from './db';
 
 config();
 
 const TOKEN = process.env.DISCORD_BOT_TOKEN as string;
+const domain = process.env.DOMAIN as string;
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -21,8 +23,43 @@ client.on('messageCreate', async (message) => {
     return;
   }
   if (message.content === '!link') {
-    await message.channel.send('こんちゃーす');
+    await createLink(message);
   }
 });
 
 client.login(TOKEN);
+
+const createLink = async (message: Message) => {
+  const existedDiscordIdQuery = `SELECT discord_id FROM users WHERE discord_id = '${message.author.id}'`;
+  const existedDiscordId = await query<{ discord_id: string }>(
+    existedDiscordIdQuery,
+  );
+  const user = message.author;
+  const linkURL = `http://${domain}/auth`;
+  if (existedDiscordId.length > 0) {
+    const description = `一応Twitterのほうのリンク貼っとくね\n[Twitter認証リンク](${linkURL})`;
+    const embed: APIEmbed = {
+      title: 'discordアカウントはもう僕には分かってるんだ',
+      description,
+      thumbnail: {
+        url: user.displayAvatarURL(),
+      },
+    };
+    await message.channel.send({ embeds: [embed] });
+    return;
+  }
+  const insertData = [user.id, user.username, user.displayAvatarURL()]
+    .map((v) => `'${v}'`)
+    .join(',');
+  const insertQuery = `INSERT INTO users (discord_id, discord_user_name, discord_avatar_url) VALUES (${insertData})`;
+  await query(insertQuery);
+  const description = `下のリンクからTwitterアカウントの認証をしてほしいんじゃな～\n[Twitter認証リンク](${linkURL})`;
+  const embed: APIEmbed = {
+    title: 'discordアカウントは分かったぜ！',
+    description,
+    thumbnail: {
+      url: user.displayAvatarURL(),
+    },
+  };
+  await message.channel.send({ embeds: [embed] });
+};
