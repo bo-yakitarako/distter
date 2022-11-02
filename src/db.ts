d73eec63b31e70beffd5d7d8bf9e2a0c6a817133
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2022-11-02 20:58:07 +0900

    がぞうひょうじしよ

--- src/db.ts
+++ src/db.ts
@@ -19,3 +19,34 @@ export async function query<T>(query: string) {
     await client.end();
   }
 }
+
+type UserColumn =
+  | 'user_id'
+  | 'discord_id'
+  | 'discord_user_name'
+  | 'discord_avatar_url'
+  | 'twitter_id'
+  | 'twitter_screen_name'
+  | 'twitter_user_name'
+  | 'twitter_avatar_url'
+  | 'encrypted_access_token'
+  | 'encrypted_access_token_secret';
+
+type UserResult = { [key in UserColumn]: string };
+
+export const select = async (
+  discord_id: string | undefined,
+  columns: UserColumn[] = [],
+) => {
+  const selectColumns = columns.length === 0 ? '*' : columns.join(', ');
+  const queryString = `SELECT ${selectColumns} FROM users WHERE discord_id = '${discord_id}'`;
+  try {
+    const result = await query<UserResult>(queryString);
+    if (result.length === 0) {
+      return null;
+    }
+    return result[0];
+  } catch {
+    return null;
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import session from 'express-session';
 import md5 from 'md5';
 import './bot';
+import { select } from './db';
 import { discordCallback } from './discord';
 import { auth, callback } from './twitter';
 
 const server = express();
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+server.set('veiw engine', 'ejs');
 server.use(
   session({
     secret: md5(Math.random().toString()),
@@ -30,8 +32,17 @@ router.get('/', (req, res) => {
   res.render('../ejs/index.ejs');
 });
 
-router.get('/linked', (req, res) => {
-  res.render('../ejs/linked.ejs');
+router.get('/linked', async (req, res) => {
+  const { discord_id } = req.session;
+  const data = await select(discord_id, [
+    'discord_avatar_url',
+    'twitter_avatar_url',
+  ]);
+  if (data === null) {
+    res.redirect('/');
+    return;
+  }
+  res.render('../ejs/linked.ejs', data);
 });
 
 server.use('/', router);
